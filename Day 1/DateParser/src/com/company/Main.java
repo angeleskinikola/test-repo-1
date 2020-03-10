d9df9b7ab5f92805c3f2e3c69c13e5f58c149d11
angeleskinikola <46307266+angeleskinikola@users.noreply.github.com>
2020-03-10 02:27:07 +0100

    Update Main.java

--- Day 1/DateParser/src/com/company/Main.java
+++ Day 1/DateParser/src/com/company/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+	    
         System.out.println("Enter MM/DD/YYYY");
 
-	String str = new Scanner(System.in).nextLine();
-	String[] string = str.split("/");
+	String date = new Scanner(System.in).nextLine();
+	String[] string = date.split("/");
 	    
 	String month = string[0];
 	String day = string[1];
