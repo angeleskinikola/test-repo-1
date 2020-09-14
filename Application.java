df0a4a306c7891b0c3647a5cac8a9efa3ef3fe0c
angeleskinikola <46307266+angeleskinikola@users.noreply.github.com>
2020-09-14 17:02:02 +0200

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Write something:");
+        String sentence = (new Scanner(System.in)).nextLine();
+        char[] chars = sentence.toCharArray();
+
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c: chars) {
+            if(map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+
+        System.out.println("Characters: appearances ");
+        for(Character c: map.keySet()) {
+            System.out.println(c + ": " + map.get(c));
+        }
+    }
+}
